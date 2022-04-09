8019f5d5436f91a7b85235fec439912a903de902
ammadkh <ammadhassan104@gmail.com>
2022-04-09 11:10:53 +0500

    cosmatic-change

--- src/features/settings/screens/favourites.screen.js
+++ src/features/settings/screens/favourites.screen.js
@@ -1,35 +1,51 @@
 import React, { useContext } from "react";
-import { FlatList } from "react-native";
+import { FlatList, View } from "react-native";
+import styled from "styled-components/native";
 import { SafeArea } from "../../../components/safeArea";
 import { Spacer } from "../../../components/spacer";
+import { Text } from "../../../components/typography";
 import { FavouriteContext } from "../../../services/favourites/favourites.context";
 import { RestaurantInfoCard } from "../../restaurants/components/restaurant-info-card";
 import { CardContainer } from "../../restaurants/screens/restaurants.styles";
+
+const NoFavScreen = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
 
 export const FavouritesScreen = ({ navigation }) => {
   const { favourites } = useContext(FavouriteContext);
   return (
     <SafeArea>
-      <Spacer position="top" size="large">
-        <FlatList
-          data={favourites}
-          keyExtractor={(item) => item.name}
-          renderItem={({ item }) => (
-            <Spacer position="bottom" size="large">
-              <CardContainer>
-                <RestaurantInfoCard
-                  restaurant={item}
-                  onPress={() =>
-                    navigation.navigate("RestaurantDetail", {
-                      restaurant: item,
-                    })
-                  }
-                />
-              </CardContainer>
-            </Spacer>
-          )}
-        />
-      </Spacer>
+      {favourites.length ? (
+        <Spacer position="top" size="large">
+          <FlatList
+            data={favourites}
+            keyExtractor={(item) => item.name}
+            renderItem={({ item }) => (
+              <Spacer position="bottom" size="large">
+                <CardContainer>
+                  <RestaurantInfoCard
+                    restaurant={item}
+                    onPress={() =>
+                      navigation.navigate("RestaurantDetail", {
+                        restaurant: item,
+                      })
+                    }
+                  />
+                </CardContainer>
+              </Spacer>
+            )}
+          />
+        </Spacer>
+      ) : (
+        <NoFavScreen
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <Text variant="label">No favourite selected yet.</Text>
+        </NoFavScreen>
+      )}
     </SafeArea>
   );
 };
